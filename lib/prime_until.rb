060a44ae9a07064c66edea48439fe86d985266a5
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-11-22 11:44:05 -0800

    add check to see if there are prime numbers before input number

--- lib/prime_until.rb
+++ lib/prime_until.rb
@@ -1,19 +1,23 @@
 def prime_until(input_number)
   num = input_number
-  prime_array = [2]
+  prime_array = []
   i = 3
   until i > num
     prime_check = true
     prime_array.each do |prime|
-      if i% prime == 0
+      if i % prime == 0
         prime_check = false
+        break
       end
     end
-
     if prime_check == true
       prime_array.push(i)
     end
     i += 2
   end
-  prime_array
+  if num < 2
+    false
+  else
+    prime_array.insert(0,2)
+  end
 end
--- spec/prime_until_spec.rb
+++ spec/prime_until_spec.rb
@@ -9,4 +9,8 @@ describe('prime_until') do
     expect(prime_until(20)).to(eq([2,3,5,7,11,13,17,19]))
     expect(prime_until(50)).to(eq([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]))
   end
+
+  it("the 'prime_until' method returns false for any number below 2 (the first prime number).") do
+    expect(prime_until(1)).to(eq(false))
+  end
 end
